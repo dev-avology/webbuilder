29a578beb9f95f2a1b918815eb20712508bca360
Nishant Dubey <nishant.avology@gmail.com>
2024-06-03 16:18:05 +0530

    fix

--- script/resources/views/welcome.blade.php
+++ script/resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
               <div class="col-lg-5">
                   <div class="hero-content">
                       <h2>Coming Soon..</h2>
-                      <p>Coming Soon is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
                        
                         <div class="hero-small-message">
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
